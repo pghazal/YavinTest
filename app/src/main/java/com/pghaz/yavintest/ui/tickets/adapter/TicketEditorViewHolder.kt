9f73496712edc5041ef557ddb69bc9eccde277dd
Pierre Ghazal <pierre.ghazal@airweb.fr>
2021-12-30 18:24:28 +0100

    Enable/disable save button if fields changed

--- app/src/main/java/com/pghaz/yavintest/ui/tickets/adapter/TicketEditorViewHolder.kt
+++ app/src/main/java/com/pghaz/yavintest/ui/tickets/adapter/TicketEditorViewHolder.kt
@@ -1,5 +1,7 @@
 package com.pghaz.yavintest.ui.tickets.adapter
 
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.recyclerview.widget.RecyclerView
 import com.pghaz.yavintest.databinding.ItemTicketEditorBinding
 import com.pghaz.yavintest.model.TicketWithQuantity
@@ -21,9 +23,50 @@ class TicketEditorViewHolder(private val binding: ItemTicketEditorBinding) :
                 )
 
                 listener.updateTicket(newTicket)
+                binding.saveButton.isEnabled = false
             } else {
                 listener.showEditNotAllowed()
             }
         }
+
+        binding.titleEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                binding.saveButton.isEnabled = shouldEnableSaveButton(
+                    ticket,
+                    s.toString(),
+                    binding.amountEditText.text.toString()
+                )
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+
+        binding.amountEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                binding.saveButton.isEnabled = shouldEnableSaveButton(
+                    ticket,
+                    binding.titleEditText.text.toString(),
+                    s.toString()
+                )
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+    }
+
+    private fun shouldEnableSaveButton(
+        ticket: TicketWithQuantity,
+        editTitle: String,
+        editAmount: String
+    ): Boolean {
+        return editTitle != ticket.title || editAmount != ticket.amount
     }
 }
