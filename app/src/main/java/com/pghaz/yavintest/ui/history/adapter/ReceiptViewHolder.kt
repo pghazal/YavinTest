28e3bf65390279f04a470ecd4f30c36c70a1ec41
Pierre Ghazal <pierre.ghazal@airweb.fr>
2021-12-31 11:26:55 +0100

    Few text styling

--- app/src/main/java/com/pghaz/yavintest/ui/history/adapter/ReceiptViewHolder.kt
+++ app/src/main/java/com/pghaz/yavintest/ui/history/adapter/ReceiptViewHolder.kt
@@ -3,13 +3,13 @@ package com.pghaz.yavintest.ui.history.adapter
 import androidx.recyclerview.widget.RecyclerView
 import com.pghaz.yavintest.databinding.ItemReceiptBinding
 import com.pghaz.yavintest.model.Receipt
+import com.pghaz.yavintest.utils.CurrencyUtils
 
-class ReceiptViewHolder(private val binding: ItemReceiptBinding) :
-    RecyclerView.ViewHolder(binding.root) {
+class ReceiptViewHolder(private val binding: ItemReceiptBinding) : RecyclerView.ViewHolder(binding.root) {
     fun bind(receipt: Receipt) {
         binding.transactionIdTextView.text = receipt.transactionId
-        binding.amountTextView.text = receipt.amount
-        binding.totalAmountTextView.text = receipt.totalAmount
+        binding.amountTextView.text =  "Amount = ${CurrencyUtils.convertCtsToDecimal(receipt.amount)}"
+        binding.totalAmountTextView.text = "Total = ${CurrencyUtils.convertCtsToDecimal(receipt.totalAmount)}"
         binding.statusTextView.text = receipt.status?.name?.uppercase()
     }
 }
